b741edd7108cdc3e91d28da17919da0af8750b82
easysir <easysir10@163.com>
2021-11-17 16:21:43 +0800

    完美矩形

---
+++ src/main/java/leetcode/everyday/No391PerfectRectangle.java
@@ -0,0 +1,94 @@
+package leetcode.everyday;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author hwang
+ * @date 2021/11/17
+ * @description 完美矩形
+ */
+public class No391PerfectRectangle {
+    public static void main(String[] args) {
+        int[][] rectangles = new int[][]{{1, 1, 3, 3}, {3, 1, 4, 2}, {3, 2, 4, 4}, {1, 3, 2, 4}, {2, 3, 3, 4}};
+        //int[][] rectangles = new int[][]{{0, 0, 1, 1}, {0, 1, 3, 2}, {1, 0, 2, 2}};
+        System.out.println(isRectangleCover(rectangles));
+    }
+
+    /**
+     * 给你一个数组 rectangles ，其中 rectangles[i] = [xi, yi, ai, bi] 表示一个坐标轴平行的矩形。这个矩形的左下顶点是 (xi, yi) ，右上顶点是 (ai, bi) 。
+     * 如果所有矩形一起精确覆盖了某个矩形区域，则返回 true ；否则，返回 false 。
+     *
+     * @param rectangles 顶点坐标
+     * @return 是否完美矩形
+     */
+    public static boolean isRectangleCover(int[][] rectangles) {
+        long area = 0;
+        int minX = rectangles[0][0], minY = rectangles[0][1], maxX = rectangles[0][2], maxY = rectangles[0][3];
+        Map<Point, Integer> cnt = new HashMap<>(1);
+        for (int[] rect : rectangles) {
+            int x = rect[0], y = rect[1], a = rect[2], b = rect[3];
+            area += (long) (a - x) * (b - y);
+
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+            maxX = Math.max(maxX, a);
+            maxY = Math.max(maxY, b);
+
+            Point point1 = new Point(x, y);
+            Point point2 = new Point(x, b);
+            Point point3 = new Point(a, y);
+            Point point4 = new Point(a, b);
+
+            cnt.put(point1, cnt.getOrDefault(point1, 0) + 1);
+            cnt.put(point2, cnt.getOrDefault(point2, 0) + 1);
+            cnt.put(point3, cnt.getOrDefault(point3, 0) + 1);
+            cnt.put(point4, cnt.getOrDefault(point4, 0) + 1);
+        }
+
+        Point pointMinMin = new Point(minX, minY);
+        Point pointMinMax = new Point(minX, maxY);
+        Point pointMaxMin = new Point(maxX, minY);
+        Point pointMaxMax = new Point(maxX, maxY);
+        if (area != (long) (maxX - minX) * (maxY - minY) || cnt.getOrDefault(pointMinMin, 0) != 1 || cnt.getOrDefault(pointMinMax, 0) != 1 || cnt.getOrDefault(pointMaxMin, 0) != 1 || cnt.getOrDefault(pointMaxMax, 0) != 1) {
+            return false;
+        }
+
+        cnt.remove(pointMinMin);
+        cnt.remove(pointMinMax);
+        cnt.remove(pointMaxMin);
+        cnt.remove(pointMaxMax);
+
+        for (Map.Entry<Point, Integer> entry : cnt.entrySet()) {
+            int value = entry.getValue();
+            if (value != 2 && value != 4) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int hashCode() {
+        return x + y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Point) {
+            Point point2 = (Point) obj;
+            return this.x == point2.x && this.y == point2.y;
+        }
+        return false;
+    }
+}
