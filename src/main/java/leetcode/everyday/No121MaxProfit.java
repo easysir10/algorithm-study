28f85c0a71e8d1644522dadc0edf416571199803
easysir <easysir10@163.com>
2021-11-18 17:28:29 +0800

    买卖股票的最佳时机I

---
+++ src/main/java/leetcode/everyday/No121MaxProfit.java
@@ -0,0 +1,51 @@
+package leetcode.everyday;
+
+/**
+ * @author hwang
+ * @date 2021/11/18
+ * @description 买卖股票的最佳时机I
+ */
+public class No121MaxProfit {
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+
+    /**
+     * 超时：给定一个数组 prices ，它的第i 个元素prices[i] 表示一支给定股票第 i 天的价格。
+     * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+     * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+     *
+     * @param prices 每天的价格
+     * @return 最大利润
+     */
+    public static int maxProfitOverTime(int[] prices) {
+        int max = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                max = Math.max(max, prices[j] - prices[i]);
+            }
+        }
+        return max;
+    }
+
+    /**
+     * 动态规划：给定一个数组 prices ，它的第i个元素prices[i]表示一支给定股票第 i 天的价格。
+     * 你只能选择 某一天 买入这只股票，并选择在未来的某一个不同的日子卖出该股票。设计一个算法来计算你所能获取的最大利润。
+     * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+     *
+     * @param prices 每天的价格
+     * @return 最大的利润
+     */
+    public static int maxProfit(int[] prices) {
+        int[] dp = new int[2];
+        dp[0] = 0;
+        dp[1] = -prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            dp[0] = Math.max(dp[0], dp[1] + prices[i]);
+            dp[1] = Math.max(dp[1], -prices[i]);
+        }
+        return dp[0];
+    }
+}
---
+++ src/main/java/leetcode/everyday/No122MaxProfit.java
@@ -0,0 +1,14 @@
+package leetcode.everyday;
+
+/**
+ * @author hwang
+ * @date 2021/11/18
+ * @description 买卖股票的最佳时机II
+ */
+public class No122MaxProfit {
+
+    public int maxProfit(int[] prices) {
+
+        return 0;
+    }
+}
