5f306afad102aa55df76d540e7f8337d32946b82
wanghu <wanghu@raisecom.com>
2025-08-07 17:27:58 +0800

    perf: 增加注释

--- src/main/java/leetcode/everyday/No122MaxProfit.java
+++ src/main/java/leetcode/everyday/No122MaxProfit.java
@@ -20,11 +20,11 @@ public class No122MaxProfit {
      */
     public static int maxProfit(int[] prices) {
         int[] dp = new int[2];
-        dp[0] = 0;
-        dp[1] = -prices[0];
+        dp[0] = 0;  // 不持股
+        dp[1] = -prices[0]; // 持股
         for (int i = 1; i < prices.length; i++) {
-            dp[0] = Math.max(dp[0], dp[1] + prices[i]);
-            dp[1] = Math.max(dp[1], dp[0] - prices[i]);
+            dp[0] = Math.max(dp[0], dp[1] + prices[i]); // 不持股：昨天没持股 或者 昨天持股今天卖掉
+            dp[1] = Math.max(dp[1], dp[0] - prices[i]); // 持股：昨天持股 或者 今天买入
         }
         return dp[0];
     }
